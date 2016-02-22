52dd439cf31d2ed711308a611ac5447c5ae93f40
Niels van der Veen <n.j.van.der.veen@st.hanze.nl>
2016-02-22 17:05:14 +0100

    Added 5.71
    Added 5.71

---
+++ chapter05/star-wars/chapter05/NameGenerator.java
@@ -0,0 +1,61 @@
+
+/**
+ * This will make your starwars name according to a formula found on the internet
+ * 
+ * @author Niels van der Veen 
+ * @version 22 feb 2016
+ */
+public class NameGenerator
+{
+    // instance variables - replace the example below with your own
+    private String firstName;
+    private String lastName;
+    private String mothersName;
+    private String cityName;
+
+    /**
+     * Constructor for objects of class NameGenerator
+     * 
+     *@param   setFirstName      The input that the firstname will be set to
+     *          setLastName      The input that the lastname will be set to
+     *          setMothersName      The input that the mothersname will be set to
+     *          setCityName      The input that the cityname will be set to
+     */
+    public NameGenerator(String setFirstName, String setLastName, String setMothersName, String setCityName)
+    {
+        firstName = setFirstName;
+        lastName = setLastName;
+        mothersName = setMothersName;
+        cityName = setCityName;
+    }
+    
+    /**
+     * This method will set all the names needed to make your starwars name.
+     * 
+     * @param   setFirstName      The input that the firstname will be set to
+     *          setLastName      The input that the lastname will be set to
+     *          setMothersName      The input that the mothersname will be set to
+     *          setCityName      The input that the cityname will be set to
+     */
+    public void setName(String setFirstName, String setLastName, String setMothersName, String setCityName)
+    {
+        firstName = setFirstName;
+        lastName = setLastName;
+        mothersName = setMothersName;
+        cityName = setCityName;
+    }
+
+    /**
+     * This will print out your starwarsname.
+     * 
+     */
+    public void generateStarWarsName()
+    {
+        // put your code here
+        String starFirst;
+        String starLast;
+        starFirst = lastName.substring(0,3) + firstName.toLowerCase().substring(0,2);
+        starLast = mothersName.substring(0,2) + cityName.toLowerCase().substring(0,3);
+        System.out.println(starFirst + " " + starLast + ", may the force be with you!");
+    }
+}
