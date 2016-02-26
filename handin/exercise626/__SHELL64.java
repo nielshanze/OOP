88b1a4a69f2d484d12e629e508b8ef916bba696f
Niels van der Veen <n.j.van.der.veen@st.hanze.nl>
2016-02-26 13:37:49 +0100

    added handing
    added handing 626

---
+++ handin/exercise626/__SHELL64.java
@@ -0,0 +1,10 @@
+
+public class __SHELL64 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/Users/nielsvdv/Documents/OOP/chapter06/zuul-better");
+final Game game1 = (Game)__bluej_runtime_scope.get("game1");
+
+
+game1.play();
+
+}}
