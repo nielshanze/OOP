fd6cf0eacfed3e345855574bda1cd7af008eb76d
nielshanze <n.j.van.der.veen@st.hanze.nl>
2016-03-11 11:14:54 +0100

    Edited
    Edited

--- chapter01/lab-classes/Instructor.java
+++ chapter01/lab-classes/Instructor.java
@@ -14,15 +14,4 @@ public class Instructor extends Student
         super(fullName, studentID);
     }
   
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return 1;
-    }
 }
