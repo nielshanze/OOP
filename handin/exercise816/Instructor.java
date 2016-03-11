53db04b14066ba7adb6d5d5f45b115e1a367e69f
nielshanze <n.j.van.der.veen@st.hanze.nl>
2016-03-11 11:14:37 +0100

    Edited it
    Edited

--- handin/exercise816/Instructor.java
+++ handin/exercise816/Instructor.java
@@ -7,30 +7,11 @@
  */
 public class Instructor extends Student
 {
-    // instance variables - replace the example below with your own
-    // the student's full name
-    private Student instructor;
-     // the student's full name
-    private String name;
-    // the student ID
-    private String id;
-    // the amount of credits for study taken so far
-    private int credits;
     
     public Instructor(String fullName, String studentID)
     {
         super(fullName, studentID);
     }
   
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return 1;
-    }
+
 }
